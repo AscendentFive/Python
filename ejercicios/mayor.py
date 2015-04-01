725d3ece5215fee74a46930d05f17f88c0cf90cd
AscendentFive <ascendentfive@gmail.com>
2015-03-31 23:06:45 -0400

    Mas Ejercicios

---
+++ ejercicios/mayor.py
@@ -0,0 +1,9 @@
+numero = int(raw_input("Ingrese 2 numeros para saber cual es mayor: "))
+numero2 = int(raw_input(": "))
+
+if numero == numero2:
+	print "Son Iguales"
+elif numero > numero2:
+	print "El Mayor es:",numero
+elif numero2 > numero:
+	print "El Mayor es:",numero2
---
+++ ejercicios/vocalono.py
@@ -0,0 +1,16 @@
+letra = raw_input("Ingrese una Letra para saber si es vocal o no: ")
+
+v = ["a", "e", "i", "o", "u"]
+
+if letra == v[0]:
+	print True
+elif letra == v[1]:
+	print True
+elif letra == v[2]:
+	print True
+elif letra == v[3]:
+	print True
+elif letra == v[4]:
+	print True
+else:
+	print False
